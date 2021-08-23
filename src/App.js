9cc7676d32ea501bc58b458812617eca3d32b846
PC Coornea <physicoornea@pop-os.localdomain>
2021-08-23 13:03:37 +0700

    add cart page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Headers from "./components/Navbar/Headers";
 import Home from "./pages/Home";
-// import PrivateRoute from './components/Route/PrivateRoute';
+import Profile from "./pages/Profile";
+import PrivateRoute from "./components/Route/PrivateRoute";
+import AddProduct from "./pages/AddProduct";
+import AddTopping from "./pages/AddTopping";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <Router>
       <Headers />
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={Home} />
-        {/* <PrivateRoute exact path='/profile' component={Profile} /> */}
-      </Switch>
+      <div style={{ marginTop: "120px" }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Home} />
+          <Route exact path="/cart" component={Cart} />
+          <PrivateRoute exact path="/product/:id" component={Product} />
+          <PrivateRoute exact path="/addProduct" component={AddProduct} />
+          <PrivateRoute exact path="/addTopping" component={AddTopping} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+        </Switch>
+      </div>
     </Router>
   );
 }
